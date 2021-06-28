8241272c650e4153821c374d6d934e12b2cb6830
Tomasz.Porst <Tomasz.Porst@adtechfactory.com>
2021-06-28 15:17:30 +0200

    working multiplayer

--- src/tic-tac-toe/TicTacToe.js
+++ src/tic-tac-toe/TicTacToe.js
@@ -1,41 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 import './TicTacToe.css';
-import { Button, Card, Row, Col, img ,CollectionItem,Collection,Carousel,Parallax } from 'react-materialize';
+import { Button, Row, Col } from 'react-materialize';
 import NotificationBadge from 'react-notification-badge';
 import {Effect} from 'react-notification-badge';
 import background from "./background.png";
 import arrow from "./arrow.png";
 
-//Brick x={this.props.x}, y={this.props.y} <Button onClick={this.onTic}/>
+//component representing a single brick on the toc tac toe borad
 class Brick extends React.Component {
 
   constructor(props) {
     super(props);
     this.onTic = this.onTic.bind(this); // always when you use a method as event handler callback you have to bind it, otherweise the this is lost  
   }    
+  // this onClick handler is liftig up the state to the main component
   onTic(e){
     // tic only if the value is not set yet  
     if(!this.props.value && !this.props.blocked){
-
+  
       this.props.onTic(this.props.x, this.props.y, e);
     }
   } 
 
   render(){
-
     let hero = this.props.value ? this.props.heros.get(this.props.value) : '';
-
     return(
       <div className="brick">        
         <div        
             className="hero"
             onClick={(e)=>this.onTic(e)}
         >       
-          {hero}
-          
+          {hero}          
         </div>
       </div>
     )
@@ -52,12 +49,11 @@ class TicTacToe extends React.Component {
     let heroIndex= Math.floor(Math.random() * 3);
     let allHerosX=['h1.png','h2.png','h3.png'];
     let allHerosY=['h4.png','h5.png','h6.png'];
-
-
+    
     this.state = {
-      owins: 0,
+      owins: 0, //score counter
       xwins:0,
-      blocked:false,
+      blocked:false, //bloking the click event if oponen is doing a move
       heros:  new Map([
         ['x', <img  src={allHerosX[heroIndex]} alt="" className="hero" />],
         ['o', <img  src={allHerosY[heroIndex]}  alt="" className="hero" />]
@@ -74,17 +70,12 @@ class TicTacToe extends React.Component {
     this.reset = this.reset.bind(this);
     this.getInventoryRow = this.getInventoryRow.bind(this);
     this.autoTic =  this.autoTic.bind(this);
-
-    
   }
 
-  reset(){
-
+reset(){
     for(let x =0; x<=2; x++){
       document.querySelectorAll('#row'+x).forEach(elem => 
         {
-          //elem.querySelector('div').querySelector('div').className +=' winner'
-        
           elem.querySelector('div').querySelector('div').className = elem.querySelector('div').querySelector('div').className.replaceAll(" winner", "")
         }); 
       document.querySelectorAll('#column'+x).forEach(elem => elem.querySelector('div').querySelector('div').className = elem.querySelector('div').querySelector('div').className.replaceAll(" winner", "")); 
@@ -100,96 +91,214 @@ class TicTacToe extends React.Component {
 
   }
 
-  incrementWin(){
-
-  }
+//asynchron move, done by auto player
+async autoTic(){ 
+    
   
-  async autoTic(x,y){
     // perform auto move after some time
     setTimeout(()=>{
-      // perform auto move
+     
+      //calculate best position
+      let [x,y] = this.calculateBestMove();
+      //perform auto move
       this.handleTic(x,y,false);
-      // unblock the normal player
+      //unblock the oposite player
       this.setState((state, props)=>{
           return {blocked: !state.blocked}
         });
-    }, 700);
+      
+    }, 600);
 
-    return 'tets autoTic';
+    return 'autoTic';
+}
+
+//deep colone of move matrix
+matrixClone(){ 
+  let copyMatrixRows=[];
+  this.state.matrix.forEach((row, rowIndex)=>{
+    copyMatrixRows[rowIndex]=[...row];
+  });
+
+  return copyMatrixRows;
+}
+//filter all dirtty values from state like 'undefined' etc. 
+//these values are automaticly created by js when we inserting into empty array in other than 1 position
+movesDone(row){
+  return  row.filter(e=>e ==='o' || e ==='x').length;
+}
+
+//check if all possible moves are done
+gameOver(matrix){
+  const reducer = (length, row) => {return length + row.filter(e => {return e ==='o' || e ==='x'}).length; };
+  let reduced = matrix.reduce(reducer,0);
+  return reduced === 9;
+}
+
+
+
+calculateBestMove(){
+  //next move ranking map
+  //always take 100 when not availabe then -100 else 0
+   let moves=  new Map([
+    [100, []],
+    [0, []],
+    [-100, []] 
+    
+  ]);
+  // clone matrix for row evaluation
+  let copyMatrixRows= this.matrixClone();
+  //calculate next move ranking for all possible moves placed in rows
+  copyMatrixRows.forEach((row, rowIndex)=>{
+    
+    for(let i=0; i<=2; i++){
+        
+      if(this.movesDone(row)===2){
+      //2 moves done, the next move can decide about win
+        if(row[i] !== 'x' && row[i] !== 'o'){
+            //check if possible to win for auto player
+            row[i]=this.state.activeFigure;        
+            if(this.evaluateTicRow(row, this.state.activeFigure)){    
+              //move to win
+              moves.set(100, [i,rowIndex] );
+            }
+            // check if possible to win for oponent
+            let nextFigure = (this.state.activeFigure === 'x' ? 'o' : 'x');
+            row[i]=nextFigure;
+              
+            if(this.evaluateTicRow(row, nextFigure)){
+              //block opponent winning move
+              moves.set(-100, [i,rowIndex] );
+              console.log('step2: i should block');           
+            }            
+
+            //no deciding move can be done
+            if(moves.get(-100).length === 0 && moves.get(-100).length === 0 ){
+              moves.set(0, [i,rowIndex] ); 
+            }
+
+          }     
+      }
+      //only 1 or no moves done in the row, the next move doesnt metter 
+      //but we can improve the algorithm, to check hier the over next move
+     // if((moves.get(100).length<1 && moves.get(-100).length<1)){
+      else if(row[i] !== 'x' && row[i] !== 'o'){
+          moves.set(0, [i,rowIndex] );
+      }        
+    }  
+  });
+
+  // clone matrix for column evaluation
+  let copyMatrixColumns=this.matrixClone();
+  //calculate next move ranking for all possible moves placed in columns
+  for(let rowIndex=0; rowIndex<=2; rowIndex++){
+    let column=[];
+    //extract column as array
+    copyMatrixColumns.forEach(row =>{
+      column.push(row[rowIndex]);
+    });
+
+    if(this.movesDone(column)===2){
+      
+      for(let i=0; i<=2; i++){
+        if(column[i] !== 'x' && column[i] !== 'o'){       
+          //check if possible to win for auto player        
+          column[i]=this.state.activeFigure;
+          if(this.evaluateTicRow(column, this.state.activeFigure)){    
+            //move to win
+            moves.set(100,  [rowIndex,i] );
+          }
+         
+          // check if possible to win for oponent
+          let nextFigure = (this.state.activeFigure === 'x' ? 'o' : 'x');
+          column[i]=nextFigure;            
+          if(this.evaluateTicRow(column, nextFigure)){
+          //move to block opponent
+            moves.set(-100, [rowIndex,i] );
+          }
+        }   
+      }
+    }
   }
 
-  handleTic(x,y, clickEvent){
-   
-    //<img  src="minecraft.png"  alt="" className="hero" />
-   
-    if(!this.state.gameOver){
-        this.setState((state, props)=>{
-        
-          let nextFigure = (this.state.activeFigure === 'x' ? 'o' : 'x');
-          //is this state manipulation ok
-          state.matrix[y][x] = state.activeFigure;
-          let over = false;
+ //after ranging all posible moves
+ if(moves.get(100).length>0){
+    //move to win 
+    return moves.get(100);
+  }else if(moves.get(-100).length>0){
+    //when not possible to win than block opponent
+    return moves.get(-100);
+  }else{
+    //else move randomly
+    //this step can be improved
+    return moves.get(0);
+  }
+}
 
-          if(this.evaluateTicRow(state.matrix[y], state.activeFigure)){
+  handleTic(x,y, clickEvent){
+
+
+    if(!this.state.gameOver){
+        
+        let winner = false;
+
+        this.setState((prevState, props)=>{
+        
+          let nextFigure = (prevState.activeFigure === 'x' ? 'o' : 'x');
+          //is this state manipulation ok
+          prevState.matrix[y][x] = prevState.activeFigure;        
           
+
+          // evaluate if the click was a winning click and show it in UI accordingly 
+          if(this.evaluateTicRow(prevState.matrix[y], prevState.activeFigure)){
+
             document.querySelectorAll('#row'+y).forEach(elem =>  {
              
-               elem.querySelector('div').querySelector('div').className +=' winner';
-          
-          
-          
-          
-          
+               elem.querySelector('div').querySelector('div').className +=' winner';          
           });
 
-            over = true;
+          winner = true;
           }
 
-          if(this.evaluateTicColumn(state.matrix, x, state.activeFigure)){
+          if(this.evaluateTicColumn(prevState.matrix, x, prevState.activeFigure)){
 
             document.querySelectorAll('#column'+x).forEach(elem =>  elem.querySelector('div').querySelector('div').className +=' winner');
-            over = true;
-
-            
-
+            winner = true;
           }
-          if(this.evaluateTicCornedDown(state.matrix, state.activeFigure)){
+          if(this.evaluateTicCornedDown(prevState.matrix, prevState.activeFigure)){
 
            
             document.querySelectorAll('.diagonalDown').forEach(elem =>  elem.querySelector('div').querySelector('div').className +=' winner');
             
-            over = true;
+            winner = true;
           }
 
-          if(this.evaluateTicCornedUp(state.matrix, state.activeFigure)){
+          if(this.evaluateTicCornedUp(prevState.matrix, prevState.activeFigure)){
 
            
             document.querySelectorAll('.diagonalUp').forEach(elem =>  elem.querySelector('div').querySelector('div').className +=' winner');
          
-            over = true;
+            winner = true;
           }
 
+          
+         
           return{
-            owins: (state.activeFigure === 'o' && over) ? state.owins+1 : state.owins,
-            xwins: (state.activeFigure === 'x' && over) ? state.xwins+1 : state.xwins,
+            owins: (prevState.activeFigure === 'o' && winner) ? prevState.owins+1 : prevState.owins,
+            xwins: (prevState.activeFigure === 'x' && winner) ? prevState.xwins+1 : prevState.xwins,
             blocked:true, //block the move and wait on auto move
-            matrix : state.matrix,
+            matrix : prevState.matrix,
             activeFigure : nextFigure,
-            gameOver : over
+            gameOver : this.gameOver(prevState.matrix)
           }
         });
 
-        
-        if(clickEvent){
-          //do auto move only after click event
-          this.autoTic(x+1,y+1);
-          
+        //do auto move only after click event
+        if(clickEvent) {                    
+          this.autoTic();          
         }
-
-
       }      
   }  
-
+  //all helper methods used to evaluate the done move 
   evaluateTicColumn(matrix, x, tic){
     let column = [];
     matrix.forEach(row=>{column.push(row[x]);});
@@ -246,6 +355,7 @@ class TicTacToe extends React.Component {
     return inventory;
 }
 
+  //the UI of the GAME
   render() {
     
     const cSize=0.1;
