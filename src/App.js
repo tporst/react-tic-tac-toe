df6c43a4320c3369a3f47f788708c48c757023fe
Tomasz.Porst <Tomasz.Porst@adtechfactory.com>
2021-06-28 22:36:39 +0200

    clean up app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
-import { Button, Card, Row, Col ,CollectionItem,Collection,Carousel,Parallax } from 'react-materialize';
-import { Navbar,Tabs,Tab ,Icon,NavItem} from 'react-materialize';
 import TicTacToe from './tic-tac-toe/TicTacToe.js';
 // functional component alternativ you can use the class component 
 function App() {
-   
   return (
-
     <div className="App">
        <h1>Tic Tac Minecraft</h1>       
        <div className="">
          <TicTacToe />
-       </div>
-      
-     
+       </div>     
     </div>
   );
 }
